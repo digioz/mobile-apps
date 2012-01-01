0f688d27efd73db1419c50fd645a4a62c38f07a6
DigiOz Multimedia <digioz@gmail.com>
2012-01-01 17:40:26 -0600

    Made the Yamba Application Multi Threaded

--- Android/Yamba/src/com/digioz/yamba/StatusActivity.java
+++ Android/Yamba/src/com/digioz/yamba/StatusActivity.java
@@ -3,6 +3,7 @@ package com.digioz.yamba;
 import winterwell.jtwitter.Twitter;
 import winterwell.jtwitter.TwitterException;
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -32,19 +33,37 @@ public class StatusActivity extends Activity implements OnClickListener {
 		
 		String status = editStatus.getText().toString();
 		
-		// Update status online
-		try {
-			Twitter twitter = new Twitter("student", "password");
-			twitter.setAPIRootUrl("http://yamba.marakana.com/api");
-			twitter.setStatus(status);
-			editStatus.setText("");
-			Toast.makeText(this, R.string.msgStatusUpdatedSuccessfully, Toast.LENGTH_LONG).show();
-		} catch (TwitterException e) {
-			e.printStackTrace();
-			Toast.makeText(this, R.string.msgStatusUpdateFailed, Toast.LENGTH_LONG).show();
-		}
+		new PostToTwitter().execute(status);
 		
 		Log.d("StatusActivity","Finished executing click event with status: "+status);
-		
+	}
+	
+	private class PostToTwitter extends AsyncTask<String, String, String> {
+
+		@Override
+		protected String doInBackground(String... status) {
+			String result;
+			
+			// Update status online
+			try {
+				Twitter twitter = new Twitter("student", "password");
+				twitter.setAPIRootUrl("http://yamba.marakana.com/api");
+				twitter.setStatus(status[0]);
+				
+				result = StatusActivity.this.getString(R.string.msgStatusUpdatedSuccessfully);
+			} catch (TwitterException e) {
+				e.printStackTrace();
+				result = StatusActivity.this.getString(R.string.msgStatusUpdateFailed);
+			}
+			
+			return result;
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			super.onPostExecute(result);
+			
+			Toast.makeText(StatusActivity.this, result, Toast.LENGTH_LONG).show();
+		}	
 	}
 }
