c5e22638861cb12f06877bd3c533460cba8c3025
DigiOz Multimedia <digioz@gmail.com>
2012-01-10 08:07:31 -0600

    Application Object for storing preferences

---
+++ Android/Yamba/src/com/digioz/yamba/YambaApplication.java
@@ -0,0 +1,14 @@
+package com.digioz.yamba;
+
+import android.app.Application;
+
+public class YambaApplication extends Application{
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		
+		
+	}
+
+}
